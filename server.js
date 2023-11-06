a05a7967196ef47b697b4e3fba58b4c6e01e1a7d
akshitbusa <akshitbusa2002@gmail.com>
2023-11-06 18:45:00 +0530

    remove unneccesary code

--- server.js
+++ server.js
@@ -8,34 +8,22 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    // res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
 
 //routes
-
-
 app.get('/', (req, res) => {
     Allbooks.find({}).then((result) => {
-        console.log('====================================');
-        console.log("res", result);
-        console.log('====================================');
         res.send(result)
     });
 })
 
 app.post('/submit', (req, res) => {
     const { book, author, summary } = req.body;
-    console.log('====================================');
-    console.log(
-        'huhhuh', book, author, summary);
-    console.log('====================================');
-
     const newBook = new Allbooks({ title: book, author: author, summary: summary });
     newBook.save()
         .then((result) => {
-            console.log('New book added:', result);
             res.status(200).json({ message: 'Book added successfully', book: result });
         })
         .catch((error) => {
@@ -47,15 +35,8 @@ app.post('/submit', (req, res) => {
 
 app.post('/edit', async (req, res) => {
     const { book, author, summary, dataid } = req.body;
-    console.log('====================================');
-    console.log(
-        'huhhuh', book, author, summary, dataid);
-    console.log('====================================');
-
     const data = await Allbooks.findByIdAndUpdate({ _id: dataid }, { title: book, author: author, summary: summary });
-
     if (data) {
-        console.log('edit data:', data);
         res.send('Data edit successfully');
     } else {
         res.status(404).send('Data not found');
@@ -73,7 +54,6 @@ app.post('/delete', async (req, res) => {
     try {
         const datadelete = await Allbooks.findOneAndDelete({ _id: data.id });
         if (datadelete) {
-            console.log('Deleted data:', datadelete);
             res.send('Data deleted successfully');
         } else {
             res.status(404).send('Data not found');
@@ -90,15 +70,14 @@ app.post('/delete', async (req, res) => {
 app.post('/view', async (req, res) => {
     const data = req.body;
     try {
-        const datadelete = await Allbooks.findById({ _id: data.id })  .then((result) => {
-            console.log('New book added:', result);
+        const datadelete = await Allbooks.findById({ _id: data.id }).then((result) => {
             res.status(200).json({ message: 'Book added successfully', book: result });
         })
-        .catch((error) => {
-            console.error('Error adding new book:', error);
-            res.status(500).json({ error: 'Failed to add new book' });
-        });
-       
+            .catch((error) => {
+                console.error('Error adding new book:', error);
+                res.status(500).json({ error: 'Failed to add new book' });
+            });
+
     } catch (error) {
         console.error('Error deleting data:', error);
         res.status(500).send('Error deleting data');
@@ -110,8 +89,6 @@ app.post('/view', async (req, res) => {
 
 
 app.listen(5000, () => {
-    console.log('====================================');
-    console.log('Node api');
-    console.log('====================================');
+    console.log('server start ...');
 })
 
